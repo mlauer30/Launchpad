4b2ff58df870c3232e996587b70980290d158999
Matthew Lauer <mlauer07@gmail.com>
2019-06-25 15:19:52 -0400

    updated logout

--- logout.php
+++ logout.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 use Auth0\SDK\Auth0;
 
 $auth0 = new Auth0([
-  'domain' => 'dev-matthewlauer.auth0.com',
-  'client_id' => 'Pef23VmWTXPjYTak0rPxMfTHs7MtCqMy',
+  'domain' => 'wQy3h3WeckKrUHh9E4Tcv08c5JadCoeN',
+  'client_id' => '_-pljKGPUpULa1fzgfNAJeufnjx2m42Yg4x2k3hzAeUh9Vr48on-5xPFVkaRPbMN',
   'client_secret' => 'jM-srxz7wRI05z7qGaKblUqfIH3_b460rzbtauDo07P8h3PwAIztmBG3KSP1qeoz',
   'redirect_uri' => 'http://ec2-100-25-44-194.compute-1.amazonaws.com/launchpad/launch.php',
   'scope' => 'openid profile email',
