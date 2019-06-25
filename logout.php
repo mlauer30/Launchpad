0108637d026eed19bb475a7ea2846df6a5e6aa5f
Matthew Lauer <mlauer07@gmail.com>
2019-06-25 14:44:23 -0400

    updated url

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@ $auth0 = new Auth0([
   'domain' => 'dev-matthewlauer.auth0.com',
   'client_id' => 'Pef23VmWTXPjYTak0rPxMfTHs7MtCqMy',
   'client_secret' => 'jM-srxz7wRI05z7qGaKblUqfIH3_b460rzbtauDo07P8h3PwAIztmBG3KSP1qeoz',
-  'redirect_uri' => 'http://0.0.0.0:8000/launchpad/launch.php',
+  'redirect_uri' => 'http://ec2-100-25-44-194.compute-1.amazonaws.com/launchpad/launch.php',
   'scope' => 'openid profile email',
 ]);
 
